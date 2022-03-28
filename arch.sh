072ccc0f1b22d314c012a2b9c08855330b08cb4e
martyynas <martynass90@gmail.com>
2022-03-28 11:07:04 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,18 +1,7 @@
 
 #!/bin/bash
 
-# Arch Linux Fast Install (archfi)
-# --------------------------------
-# author    : MatMoul
-#             https://github.com/MatMoul
-#             http://sourceforge.net/u/matmoul
-# project   : https://github.com/MatMoul/archfi
-#             http://sourceforge.net/projects/archfi/
-# license   : LGPL-3.0 (http://opensource.org/licenses/lgpl-3.0.html)
-#
-# referance : https://wiki.archlinux.org/index.php/Installation_guide
-
-
+# Arch Linux Fast Install
 # --------------------------------------------------------
 mainmenu(){
 	if [ "${1}" = "" ]; then
