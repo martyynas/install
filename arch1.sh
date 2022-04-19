04c5e9f609935be472aa7c486baf1813a67cbc89
martyynas <martynass90@gmail.com>
2022-04-19 20:27:18 +0300

    Add files via upload

---
+++ arch1.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Update the system clock
+timedatectl set-ntp true
+
+# Cfdisk
+cfdisk /dev/sda
+
+# Formatting partitions
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkswap /dev/sda3
+
+# Mounting the partitions
+mount /dev/sda2 /mnt
+mkdir /mnt/efi
+mount /dev/sda1 /mnt/efi
+swapon /dev/sda3
+
+# Installation
+pacstrap /mnt base base-devel linux linux-firmware  networkmanager nano
+
+# Genfstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Chroot into the installtion
+arch-chroot /mnt
+
+
