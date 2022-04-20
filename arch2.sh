e79d5bb75067ddb349e94f5ace311401d6264569
martyynas <martynass90@gmail.com>
2022-04-20 18:13:47 +0300

    Add files via upload

---
+++ arch2.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+# Setting the timezone
+ln -sf /usr/share/zoneinfo/Europe/Vilnius /etc/localtime
+
+# Localization
+nano /etc/locale.gen
+locale-gen
+echo LANG=en_GB.UTF-8 > /etc/locale.conf
+export LANG=en_GB.UTF-8
+
+#Set your host name
+echo linuxOS > /etc/hostname
+touch /etc/hosts
+nano /etc/hosts
+
+# Root password
+passwd
+
+# Add user
+useradd -m -G wheel martynas
+passwd martynas
+nano /etc/sudoers
+
+# Network installtion
+pacman -S --noconfirm networkmanager
+systemctl enable NetworkManager 
+
+# Bootloader installtion
+pacman -S --noconfirm intel-ucode
+pacman -S --noconfirm grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=Arch-Linux
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Install xfce4
+pacman -S --noconfirm xfce4 xfce4-goodies sddm pulseaudio pavucontrol firefox
+
+# sddm service 
+systemctl enable sddm
+
