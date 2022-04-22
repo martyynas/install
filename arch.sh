9b9b16f86a6f3a51fee7ed9ae6c4050476e735f9
martyynas <martynass90@gmail.com>
2022-04-22 18:36:46 +0300

    Delete arch.sh

--- arch.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-
-devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
-device=$(dialog --stdout --menu "Select installtion disk" 0 0 0 ${devicelist}) || exit 1
-clear
-
-cfdisk "${device}"
