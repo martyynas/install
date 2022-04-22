e1b8dda755ef45d11735f1ed61916aa659441e68
martyynas <martynass90@gmail.com>
2022-04-22 18:08:26 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -6,4 +6,4 @@ devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
 device=$(dialog --stdout --menu "Select installtion disk" 0 0 0 ${devicelist}) || exit 1
 clear
 
-gdisk "${device}"
+cfdisk "${device}"
