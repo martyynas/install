bfd2201665434085ee47c2a0cc5984c522343a82
martyynas <martynass90@gmail.com>
2022-04-22 18:07:31 +0300

    Add files via upload

---
+++ arch.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+device=$(dialog --stdout --menu "Select installtion disk" 0 0 0 ${devicelist}) || exit 1
+clear
+
+gdisk "${device}"
